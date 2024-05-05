c0049b17b0acd9c6e5077e7d2d1305a0be186352
Ivan <162828351+elias1524@users.noreply.github.com>
2024-05-05 09:25:43 -0400

    Actualizar info-contacto.js

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -1,5 +1,7 @@
-// CÓDIGO CREADO GRACIAS A https://github.com/Azami19 & https://github.com/GataNina-Li
+//CÓDIGO CREADO GRACIAS A https://github.com/Azami19 & https://github.com/GataNina-Li
+//Puedes editar el país,enlaces, los números se editan desde el config.js
 
+// ARREGLADO Y ADAPTADO PARA MYLINGBOT-MD POR GL YT MX
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
@@ -19,19 +21,29 @@ let biografia = await conn.fetchStatus(number +'@s.whatsapp.net').catch(_ => 'un
 let bio = biografia.status?.toString() || `${desc2 == '' ? lenguajeMY.smsContacto2() : desc2}`
   
 nombre = official[0][0] == String(contact[0]) ? official[0][1] : official[1][0] == String(contact[0]) ? official[1][1] : official[2][0] == String(contact[0]) ? official[2][1] : official[3][0] == String(contact[0]) ? official[3][1] : official[4][0] == String(contact[0]) ? official[4][1] : lenguajeMY.smsContacto3() 
-description = official[0][0] == String(contact[0]) ? 'Solo temas de KatashiBot' : official[1][0] == String(contact[0]) ? lenguajeMY.smsContacto4() : official[2][0] == String(contact[0]) ? lenguajeMY.smsContacto4() : official[3][0] == String(contact[0]) ? lenguajeMY.smsContacto4() : desc === '' ? lenguajeMY.smsContacto5() : desc
-correo = official[0][0] == String(contact[0]) ? 'katashifukushima23@gmail.com' : official[1][0] == String(contact[0]) ? 'soeasynt@gmail.com' : official[2][0] == String(contact[0]) ? lenguajeMY.smsContacto6() : mail === '' ? lenguajeMY.smsContacto6() : mail
-lugar = official[0][0] == String(contact[0]) ? '🇵🇪 Perú' : official[1][0] == String(contact[0]) ? '🇲🇽 México' : official[2][0] == String(contact[0]) ? '🇻🇪 Venezuela' : official[3][0] == String(contact[0]) ? '🇲🇽 México' : country === '' ? '🇵🇪 Perú' : country
+description = official[0][0] == String(contact[0]) ? '𝘾𝙍𝙀𝘼𝘿𝙊𝙍 𝙊𝙁𝙄𝘾𝙄𝘼𝙇 𝘿𝙀 𝙈𝙔𝙇𝙄𝙉𝙂𝘽𝙊𝙏-𝙈𝘿' : official[1][0] == String(contact[0]) ? lenguajeMY.smsContacto4() : official[2][0] == String(contact[0]) ? lenguajeMY.smsContacto4() : official[3][0] == String(contact[0]) ? lenguajeMY.smsContacto4() : desc === '' ? lenguajeMY.smsContacto5() : desc
+correo = official[0][0] == String(contact[0]) ? '𝘚𝘐𝘕 𝘊𝘖𝘙𝘙𝘌𝘖' : official[1][0] == String(contact[0]) ? 'glyt.xp.contacto@gmail.com' : official[2][0] == String(contact[0]) ? lenguajeMY.smsContacto6() : mail === '' ? lenguajeMY.smsContacto6() : mail
+lugar = official[0][0] == String(contact[0]) ? '🇦🇷 𝘈𝘙𝘎𝘌𝘕𝘛𝘐𝘕𝘈' : official[1][0] == String(contact[0]) ? '🇦🇷 𝘈𝘙𝘎𝘌𝘕𝘛𝘐𝘕𝘈' : official[2][0] == String(contact[0]) ? '𝘚𝘐𝘕 𝘗𝘈𝘐𝘚' : official[3][0] == String(contact[0]) ? '𝘚𝘐𝘕 𝘗𝘈𝘐𝘚' : country === '' ? '🇵𝘚𝘐𝘕 𝘗𝘈𝘐𝘚' : country
 enlace = official[0][0] == String(contact[0]) ? 'https://github.com/KatashiFukushima' : official[1][0] == String(contact[0]) ? 'https://chat.whatsapp.com/J2IRY8iLvtL2Us6pVGGcRH' : official[2][0] == String(contact[0]) ? 'https://whatsapp.com/channel/0029Va8GeVFAO7REOj3qnW37' : official[3][0] == String(contact[0]) ? 'https://chat.whatsapp.com/EBQOkLPgftc8mPR4KTml8S' : md 
    
-lista.push([number, ofc, nombre, description, official[3][0] == String(contact[0]) ? null : correo, lugar, enlace, bio, official[1][0] == String(contact[0]) ? 'https://chat.whatsapp.com/J2IRY8iLvtL2Us6pVGGcRH' : null])
-lista.push([conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), packname, lenguajeMY.smsContacto8(), mail === '' ? 'katashifukushima23@gmail.com' : mail, lenguajeMY.smsContacto7(), md, yt, ig, fb, paypal, nna])
+lista.push([number, ofc, nombre, description, official[3][0] == String(contact[0]) ? null : correo, lugar, enlace, bio, official[1][0] == String(contact[0]) ? 'https://chat.whatsapp.com/J2IRY8iLvtL2Us6pVGGcRH' : null]) }  
+lista.push([conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), packname, lenguajeMY.smsContacto8(), mail === '' ? 'katashifukushima23@gmail.com' : mail, lenguajeMY.smsContacto7(), md, bioBot, yt, ig, fb, paypal, nna])
 await conn.sendContactArray(m.chat, lista, null, { quoted: fkontak })
   
 } catch (e) {
 await m.reply(lenguajeMY['smsMalError3']() + '\n*' + lenguajeMY.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeMY.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeMY.smsMensError2()} ` + usedPrefix + command)
 console.log(`❗❗ ${lenguajeMY['smsMensError2']()} ${usedPrefix + command} ❗❗`)
 console.log(e)}} 
-handler.command = /^(contacto?|contacts?)$/i //['contacto', 'contact']  
+handler.command = /^(contactos?|contacts?)$/i //['contacto', 'contact']  
 
 export default handler
+
+
+/*function handler(m) {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+const data = global.owner.filter(([id, isCreator]) => id && isCreator) 
+this.sendContact(m.chat, data.map(([id, name]) => [id, name]), fkontak, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
+}
+
+handler.command = ['contacto', 'contact']  
+export default handler*/

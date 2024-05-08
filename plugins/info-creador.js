0805ea65a21487aaa663bcabeb56f2a663d97b64
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-05-08 07:26:39 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -2,43 +2,24 @@ var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let pp = creatorimg.getRandom()        
 const cat = `
-彡 ⪻ ⏤͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞𝐌𝐲𝐥𝐢𝐧𝐠𝐁𝐨𝐭-𝐌𝐃 ⪼ 彡
+
+𝑮𝑹𝑨𝑪𝑰𝑨𝑺 𝑨 𝑳𝑶𝑺 𝑪𝑶𝑳𝑨𝑩𝑶𝑹𝑨𝑫𝑶𝑹𝑬𝑺 𝑫𝑬 𝑬𝑺𝑻𝑬 𝑷𝑹𝑶𝒀𝑬𝑪𝑻𝑶 𝑷𝑶𝑹 𝑨𝒀𝑼𝑫𝑨𝑹𝑴𝑬 𝒀 𝑫𝑨𝑹𝑴𝑬 𝑼𝑵𝑨 𝑴𝑨𝑵𝑶 𝑪𝑼𝑨𝑵𝑫𝑶 𝑳𝑨 𝑵𝑬𝑪𝑬𝑺𝑰𝑻𝑬
+
+𝐂𝐎𝐋𝐀𝐁𝐎𝐑𝐀𝐃𝐎𝐑𝐄𝐒:
+» 𝖣𝖨𝖤𝖦𝖮
+» 𝖠𝖷𝖷
+» 𝖨𝖵𝖠𝖭
+» 𝖤𝖭𝖣𝖤𝖱 𝖳𝖠𝖫𝖫𝖸𝖢𝖫𝖤𝖱𝖪
+
+𝐂𝐑𝐄𝐀𝐃𝐎𝐑:
+» 𝖦𝖫 𝖸𝖳 𝖬𝖷
+
+𝐈𝐍𝐅𝐎:
+» 𝖣𝖨𝖤𝖦𝖮
+» gitHub.com/
 
 
- ◆ ⃟ ⃟ ░▒▓  ҈ ҈ ҈ ҈ ⃟ ♧ ⃟  ҈ ҈ ҈ ҈▓▒░ ⃟ ⃟ ◆ 
---- 𝙲𝚁𝙴𝙰𝚃𝙴𝙳 𝙱𝚈 𝚆𝙸𝙻𝚂𝙾𝙽  𝙾𝙵𝙲---
-◆ ⃟ ⃟ ░▒▓  ҈ ҈ ҈ ҈ ⃟ ♧ ⃟  ҈ ҈ ҈ ҈▓▒░ ⃟ ⃟
-
-    ★彡𝑰𝑵𝑭𝑶 𝑪𝑹𝑬𝑨𝑫𝑶𝑹彡★
-♥️🎩⚠︎  シ︎𝑾𝒊𝒍𝖘𝒐𝒏シ︎ ⚠︎🎩♥️
-    • *${asistencia}*
-   
-- 𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌:
-  *${ig}*
-
-- 𝐂𝐀𝐍𝐀𝐋 𝐃𝐄 𝐘𝐓:
-  *${yt}*
-
-- 𝐂𝐀𝐍𝐀𝐋 𝐎𝐅𝐂 𝐌𝐘:
-  *${cnl}*
-
-- 𝐑𝐄𝐏𝐎𝐒𝐈𝐓𝐎𝐑𝐈𝐎:
-  *${md}*
-
-⊰᯽⊱┈─╌❊ - ❊╌─┈⊰᯽⊱
-
- ╔══❖•ೋ° - °ೋ•❖══╗
-- *¿TIENES DUDAS SOBRE EL BOT?*
-
-- *¿TIENES ALGUNA QUEJA CON EL BOT?* 
-
-- *¿TE GUSTARIA COMPARTIR OPINIONES Y/O SUGERENCIAS DEL BOT?*
-
-
-*¡PERFECTO!*
-> CONTACTA CON MI CREADOR PARA CUALQUIER TEMA DEL BOT, NO ESCRIBIR SI NO ES POR TEMA DEL BOT, EVITE PROBLEMAS.
-
-╚══❖•ೋ° - °ೋ•❖══╝`
+`
 
 await conn.sendFile(m.chat, pp, 'Hola.mp3', cat, fkontak)
 }

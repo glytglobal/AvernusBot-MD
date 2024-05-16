a2a5a35cbd4a7bdeed21e657ec92c22c577d0781
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-05-16 09:02:40 -0300

    𝐀𝐯𝐞𝐫𝐧𝐮𝐬𝐁𝐨𝐭-𝐌𝐃

--- plugins/info-host.js
+++ plugins/info-host.js
@@ -1,38 +1,17 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let txt = `*\`💫 Te presentamos un nuevo hosting: "Infinity-Wa Host"\`*
+let txt = `¡TE PRESENTAMOS!
 
-*¿Muy lento tu nokia y necesitas tener activo tu bot 24/7?*
+_¡BIENVENIDO/A A BOXMINE HOST!_
 
-> *Te tenemos la mejor opción para mantener activo tu bot 24/7, a precios muy accesibles. Es muy barato y todos pueden comprar.*
+> UN LUGAR ÚNICO Y UN BUEN SERVICIO DE ATENCIÓN PARA LOS USUARIOS, CONTAMOS CON SERVICIOS GRATUITOS Y DE PAGA ADEMÁS DE QUE CONTAMOS CON UN SERVICIO DE ASISTENCIA RÁPIDA Y PERSONALIZADA
 
-*🚩Precios :*
-* 1GB, 100 CPU = 1Dolar
-* 2GB, 120 CPU = 2Dolar
-* 3GB, 140 CPU = 3Dolar
-* 4GB, 175 CPU = 4Dolar
-* 5GB, 200 CPU = 5 Dólar. 
+*_❑ PAGINA ❑_*
+_» https://boxmineworld.com_
 
-🟢 \`\`\`Información del Host\`\`\`
+*_✰ PAGINA - DASH ✰_*
+_» https://dash.boxmineworld.com_
+`
 
-💻 *Página:*
-https://host.panel-infinitywa.store
-
-🧡 *Canal de WhatsApp:*
-https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A
-
-💚 *Grupo:*
-https://chat.whatsapp.com/GQ82mPnSYnm0XL2hLPk7FV
-
-🛍️ *Método de pago:*
-*• PayPal :* paypal.me/OfcGB
-*• Mercado pago, alías:* OficialGB
-*• Naranja x, alías:* OficialGL
-*• Yape (Perú) :* +51948705559
-
-*\`💙 Contactanos para más información o alquidir los servicios:\`*
-• https://www.facebook.com/elrebelde21
-• wa.me/527294888993
-• wa.me/5492964650915`
 await conn.sendMessage(m.chat, { text: txt,
 contextInfo:{
 forwardingScore: 9999999,
@@ -40,11 +19,11 @@ isForwarded: false,
 "externalAdReply": {
 "showAdAttribution": true,
 "containsAutoReply": true,
-title: `🤖 𝐈𝐍𝐅𝐈𝐍𝐈𝐓𝐘𝐖𝐀-𝐇𝐎𝐒𝐓 🤖`,
-body: `✅ Hosting de Calidad`,
+title: `⏤͟͟͞͞𝐁𝐎𝐗𝐌𝐈𝐍𝐄 - 𝐖𝐎𝐑𝐋𝐃`,
+body: `✿ HOSTING DE LUJO ✿`,
 "previewType": "PHOTO",
-thumbnailUrl: 'https://qu.ax/EQTd.jpg', 
-sourceUrl: 'https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A'}}},
+thumbnailUrl: 'https://telegra.ph/file/767775d7250ae841a9cb6.jpg', 
+sourceUrl: 'https://whatsapp.com/channel/0029Va71C1q2UPBOICnxu83r'}}},
 { quoted: m})
 }
 handler.tags =['info'] 
